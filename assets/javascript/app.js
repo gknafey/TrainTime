a5d51639915f7b258af853c0cd92729d7e2dae1a
Garpken <gknafey@gmail.com>
2019-07-15 16:06:24 -0400

    modified index.html slightly to accomodate the app.js file, added the basics for the app.js to contact firebase.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyABxxJRkSWvL6lNWXigMJBlPmwxHFZh8rg",
+    authDomain: "gart-c818d.firebaseapp.com",
+    databaseURL: "https://gart-c818d.firebaseio.com",
+    projectId: "gart-c818d",
+    storageBucket: "",
+    messagingSenderId: "1095228649044",
+    appId: "1:1095228649044:web:7919ab47e90755ad"
+  };
+
